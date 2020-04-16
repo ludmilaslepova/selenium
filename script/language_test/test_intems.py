fe6f30500d2c7b0e62879d4683bf0304162d3994
Slepova Ludmila <slepova@radario.co>
2020-04-16 14:20:57 +0300

    added assert

--- script/language_test/test_intems.py
+++ script/language_test/test_intems.py
@@ -6,5 +6,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 def test_buy_button(browser):
     time.sleep(5)
-    WebDriverWait(browser, 10).until(
+    button = WebDriverWait(browser, 10).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, "[type='submit']"))) 
+    assert button.text is not None, "Error. Some string was expected"
